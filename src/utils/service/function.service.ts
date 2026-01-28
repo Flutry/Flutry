273a2ef4656c0b73f3a496acb889e3b537c9244e
HEDI <admin@otamoon.hu>
2026-01-28 08:06:27 +0100

    Refactor and enhance FunctionService utilities
    Renamed the file to correct a typo and updated the class method name to FormatZodErrors. Added error formatting and bot token verification utilities, and included logger and util imports for improved error handling and logging.

--- src/utils/service/function.service.ts
+++ src/utils/service/function.service.ts
@@ -1,6 +1,10 @@
+import { logger } from '@flutry/main';
+import util from 'node:util';
+
 export class FunctionService {
-  public static FormatZodError = (error: any) => {
+  public static FormatZodErrors = (error: any) => {
     const formattedErrors: { [key: string]: string } = {};
+
     // Zod 4.x verzióban az issues tömböt használjuk
     error.issues.forEach((issue: any) => {
       const fieldName = issue.path[0] as string;
@@ -9,6 +13,7 @@ export class FunctionService {
         formattedErrors[fieldName] = issue.message;
       }
     });
+
     return formattedErrors;
   };
 
@@ -22,4 +27,30 @@ export class FunctionService {
   };
 
   public static sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  public static errorFormat = (error: any) => {
+    return `${util.format(error)}`;
+  };
+
+  public static verifyBotToken = async (ctx: any) => {
+    try {
+      const authorization = ctx.getHeader('Authorization');
+      if (!authorization) {
+        return false;
+      }
+
+      const data = authorization.split(' ');
+      if (data[0] !== 'Bearer' || !data[1]) {
+        return false;
+      }
+
+      if (data[1] !== process.env.DISCORD_BOT_API_KEY) {
+        return false;
+      }
+      return true;
+    } catch (error) {
+      logger.warn(`Verify token failed: ${await FunctionService.errorFormat(error)}`);
+      return false;
+    }
+  };
 }
